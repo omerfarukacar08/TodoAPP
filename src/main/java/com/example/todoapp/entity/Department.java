6a38a938deaece68d1fc8ce09cb26f051eb37883
omerfarukacar08 <omerfarukacar08@gmail.com>
2026-07-21 16:56:47 +0300

    Initial commit

---
+++ src/main/java/com/example/todoapp/entity/Department.java
@@ -0,0 +1,52 @@
+package com.example.todoapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//  alanın değeri otomatik üretilecek
+
+    private Long id;// Bu alan Primary Key
+
+    private String name;
+
+    private String code;
+
+    public Department() {
+    }
+
+    public Department(Long id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
